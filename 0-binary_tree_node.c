763bb74ec8ce6ca1678d06b5981d36cc6f1478bd
YesseJr <thonnieyesse@hotmail.com>
2022-02-17 08:18:15 +0300

    New node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -5,9 +5,9 @@
  * @parent: The parent node of the new binary tree node.
  * @value: The value of the new binary tree node.
  *
+ * Author: Waython Yesse
  * Return: A pointer to the created node, otherwise NULL.
  */
-
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newnode = NULL;
