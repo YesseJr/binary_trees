071c7a6f5848c4fb59bf227d52f8d3a7a8a4274e
YesseJr <thonnieyesse@hotmail.com>
2022-02-17 08:21:55 +0300

    New node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -19,7 +19,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 		newnode->left = NULL;
 		newnode->right = NULL;
         newnode->parent = parent;
-		newnode->n = value;	
+		newnode->n = value;
 	}
 	return (newnode);
 }
